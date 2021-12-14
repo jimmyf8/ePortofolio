e1d4022b116efeed0673535e89e7c7e8edc42d22
farescourt <jimmy.farescourt@gmail.com>
2021-12-14 17:20:54 +0100

    formation

---
+++ portofolio/src/views/Formation.vue
@@ -0,0 +1,179 @@
+<template>
+  <div class="about">
+    <h1>Ma formation</h1>
+    <div class="container">
+      <div class="card" style="width: 18rem">
+        <h2>Développeur Web, chez OpenClassrooms</h2>
+        <img
+          id="oc"
+          class="card-img-top"
+          src="../assets/oc_logo.png"
+          alt="Card image cap"
+        />
+        <div class="card-body">
+          <p class="card-text">
+            RNCP Développeur web niveau bac +2 Paris (7 mois)
+          </p>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem">
+        <h2>BTS Assistant de Gestion, CNED</h2>
+        <img
+          class="card-img-top"
+          src="../assets/CNED_Logo.jpg"
+          alt="Card image cap"
+        />
+        <div class="card-body">
+          <p class="card-text">niveau 2ème année</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+};
+</script>
+
+<style scoped>
+.about {
+  height: 100vh;
+}
+.container {
+  width: 75%;
+  padding: 100px;
+  display: flex;
+  justify-content: space-between;
+  -webkit-animation: bounce-in-top 1.1s both;
+  animation: bounce-in-top 1.1s both;
+}
+img {
+  width: 400px;
+}
+#oc {
+  width: 200px;
+}
+/*media queries*/
+@media screen and (max-width: 900px) {
+  .container {
+    height: 90vh;
+    display: flex;
+    flex-direction: column;
+    padding: 0px;
+    margin-left: 100px;
+  }
+  img {
+    width: 200px;
+  }
+  #oc {
+    width: 100px;
+  }
+}
+/*animation*/
+@-webkit-keyframes bounce-in-top {
+  0% {
+    -webkit-transform: translateY(-500px);
+    transform: translateY(-500px);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+    opacity: 0;
+  }
+  38% {
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+    -webkit-animation-timing-function: ease-out;
+    animation-timing-function: ease-out;
+    opacity: 1;
+  }
+  55% {
+    -webkit-transform: translateY(-65px);
+    transform: translateY(-65px);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+  }
+  72% {
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+    -webkit-animation-timing-function: ease-out;
+    animation-timing-function: ease-out;
+  }
+  81% {
+    -webkit-transform: translateY(-28px);
+    transform: translateY(-28px);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+  }
+  90% {
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+    -webkit-animation-timing-function: ease-out;
+    animation-timing-function: ease-out;
+  }
+  95% {
+    -webkit-transform: translateY(-8px);
+    transform: translateY(-8px);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+  }
+  100% {
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+    -webkit-animation-timing-function: ease-out;
+    animation-timing-function: ease-out;
+  }
+}
+@keyframes bounce-in-top {
+  0% {
+    -webkit-transform: translateY(-500px);
+    transform: translateY(-500px);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+    opacity: 0;
+  }
+  38% {
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+    -webkit-animation-timing-function: ease-out;
+    animation-timing-function: ease-out;
+    opacity: 1;
+  }
+  55% {
+    -webkit-transform: translateY(-65px);
+    transform: translateY(-65px);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+  }
+  72% {
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+    -webkit-animation-timing-function: ease-out;
+    animation-timing-function: ease-out;
+  }
+  81% {
+    -webkit-transform: translateY(-28px);
+    transform: translateY(-28px);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+  }
+  90% {
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+    -webkit-animation-timing-function: ease-out;
+    animation-timing-function: ease-out;
+  }
+  95% {
+    -webkit-transform: translateY(-8px);
+    transform: translateY(-8px);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+  }
+  100% {
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+    -webkit-animation-timing-function: ease-out;
+    animation-timing-function: ease-out;
+  }
+}
+</style>
