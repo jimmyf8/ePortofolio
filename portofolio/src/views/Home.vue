710c309e01e0c2d10b22042eb3d1e3b757737b2a
farescourt <jimmy.farescourt@gmail.com>
2021-12-14 17:21:07 +0100

    home

---
+++ portofolio/src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+ 
+    
+    <HelloWorld msg="Bienvenue sur mon  ePortofolio"/>
+    
+    
+   
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+
+
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  
+    
+  }
+}
+</script>
+
+<style scoped>
+  .home{
+    padding: 80px;
+    height: 100hv;
+    
+  }
+  @media screen and (max-width:758px) {
+    .home{
+    padding: 0px;
+    height: 100hv;
+    
+  }
+  }
+</style>
